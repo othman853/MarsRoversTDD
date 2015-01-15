8a97a61159dac17a9dc63765442e8f32b5d56386
othman853 <anuar.yasser@gmail.com>
2015-01-15 09:22:44 -0200

    the Dialogs Class now receives a Scanner as parameters instead of creating a new one

--- src/main/java/br/com/yasser/rachid/marsrovers/main/Dialogs.java
+++ src/main/java/br/com/yasser/rachid/marsrovers/main/Dialogs.java
@@ -1,28 +1,45 @@
 package br.com.yasser.rachid.marsrovers.main;
 
+import java.io.InputStream;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Dialogs {
 	
-	private static Scanner reader;
-	private static final String POSITION_INPUT_PATTERN = "([0-9])+(,)+([0,9])+";
+	private Scanner reader;
+	public static final String POSITION_INPUT_PATTERN = "([0-9])+(,)+([0-9])+";
 	
-	public Dialogs(){
-		if(reader == null){
-			reader = new Scanner(System.in);
-		}
+	public Dialogs(Scanner reader){		
+		this.reader = reader;		
 	}
 	
-	public static ExplorationPoint askForPosition(){
+	public Dialogs(){
+		
+	}
+	
+	public void setReader(Scanner reader){
+		this.reader = reader;
+	}
+	
+	public Scanner getReader(){
+		return this.reader;
+	}
+	
+	public ExplorationPoint askForPosition() throws InputMismatchException{		
+		System.out.print("position [X,Y] > " );
+		String input = "";
+
+		input = reader.next(POSITION_INPUT_PATTERN);
+
+		return extractExplorationPoint(input);
+	}
+	
+	public ExplorationPoint extractExplorationPoint(String input){
 		int x = 0;
 		int y = 0;
 		
 		ExplorationPoint point = new ExplorationPoint();
-		
-		String input = "";
-		System.out.print("position [X,Y] > " );		
-		input = reader.next(POSITION_INPUT_PATTERN);		
-		
+				
 		x = Integer.parseInt(input.split(",")[0]);
 		y = Integer.parseInt(input.split(",")[1]);
 		
