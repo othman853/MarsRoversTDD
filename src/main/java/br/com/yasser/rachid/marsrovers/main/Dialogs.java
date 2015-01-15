a05ff9eed5c52f1cf97586434106bd07812e470d
othman853 <anuar.yasser@gmail.com>
2015-01-15 10:09:45 -0200

    askForCommand() created

--- src/main/java/br/com/yasser/rachid/marsrovers/main/Dialogs.java
+++ src/main/java/br/com/yasser/rachid/marsrovers/main/Dialogs.java
@@ -8,6 +8,7 @@ public class Dialogs {
 	
 	private Scanner reader;
 	public static final String POSITION_INPUT_PATTERN = "([0-9])+(,)+([0-9])+";
+	public static final String COMMAND_INPUT_PATTERN  = "[LMR]";
 	
 	public Dialogs(Scanner reader){		
 		this.reader = reader;		
@@ -30,6 +31,15 @@ public class Dialogs {
 		return extractExplorationPoint(input);
 	}
 	
+	public Command askForCommand() throws InputMismatchException{
+		System.out.print("command [LMR] > ");
+		
+		String input = reader.next(COMMAND_INPUT_PATTERN);
+		Command command = new Command(input);
+		
+		return command;
+	}
+	
 	public ExplorationPoint extractExplorationPoint(String input){
 		int x = 0;
 		int y = 0;
@@ -42,6 +52,5 @@ public class Dialogs {
 		point = new ExplorationPoint(x,y);
 		
 		return point;
-	}
-
+	}	
 }
