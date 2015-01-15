5a4a4fa149768fca731cbce9ff42e58dfaf9b7e9
othman853 <anuar.yasser@gmail.com>
2015-01-15 09:57:53 -0200

    Renaming old tests and creating a new one.

--- src/test/java/main/DialogsTest.java
+++ src/test/java/main/DialogsTest.java
@@ -14,15 +14,15 @@ import br.com.yasser.rachid.marsrovers.main.ExplorationPoint;
 
 public class DialogsTest {
 	
-	private Dialogs dialogs; 
+	private Dialogs dialogs;	 
 	
 	@Before
-	public void setUp(){
+	public void setUp(){		
 		dialogs = new Dialogs();
 	}
 	
 	@Test
-	public void askForPosition_ShouldReturnA5by5EXplorationPoint(){		
+	public void askForPosition_Valid5by5CoordinatesAsParam_ShouldReturnA5by5EXplorationPoint(){		
 		String expectedInput = "5,5";		
 		dialogs.setReader(createScanner(expectedInput));
 		ExplorationPoint expectedPoint = new ExplorationPoint(5,5);
@@ -33,12 +33,33 @@ public class DialogsTest {
 	}
 	
 	@Test(expected=InputMismatchException.class)
-	public void askForPosition_ShouldThrowInputMismatchException(){
+	public void askForPosition_InvalidCoordinatesAsParam_ShouldThrowInputMismatchException(){
 		String incorrectInput = "a,a";
 		Scanner reader = createScanner(incorrectInput);
 		dialogs.setReader(reader);
 		
 		dialogs.askForPosition();
+	}
+	
+	@Test
+	public void askForMoveCommand_ValidCommandAsParam_ShouldReturnAValidCommandObject(){
+		String expectedInput = "LMRMLM";
+		Scanner reader = createScanner(expectedInput);
+		dialogs.setReader(reader);
+		Command expected = new Command(expectedInput);
+		
+		Command generated = dialogs.askForCommand();
+		
+		assertEquals(generated, expected);
+	}
+	
+	@Test(expected=InputMismatchException.class)
+	public void askForMoveCommand_InvalidCommandAsParam_ShouldThrowInputMismatchException(){
+		String incorrectInput = "LMA";
+		Scanner reader = createScanner(incorrectInput);
+		dialogs.setReader(reader);
+		
+		dialogs.askForCommand();
 	}
 	
 	
