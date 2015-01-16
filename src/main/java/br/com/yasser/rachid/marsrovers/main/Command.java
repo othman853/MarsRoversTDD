06be7ea769cfcd50d840ae0984952b1bf9bc319a
othman853 <anuar.yasser@gmail.com>
2015-01-16 15:31:51 -0200

    hashCode() and equals() implemented

--- src/main/java/br/com/yasser/rachid/marsrovers/main/Command.java
+++ src/main/java/br/com/yasser/rachid/marsrovers/main/Command.java
@@ -16,4 +16,18 @@ public class Command {
 		return command.toString();
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((command == null) ? 0 : command.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Command other = (Command) obj;
+		
+		return this.command.equals(other.get());
+	}
 }
