39e26553a4634cc75ec36ccbef62eb548e06f280
othman853 <anuar.yasser@gmail.com>
2015-01-17 11:41:48 -0300

    [DUPLICATE] askForFieldSize() created

--- src/main/java/br/com/yasser/rachid/marsrovers/view/Dialogs.java
+++ src/main/java/br/com/yasser/rachid/marsrovers/view/Dialogs.java
@@ -1,13 +1,21 @@
-package br.com.yasser.rachid.marsrovers.main;
+package br.com.yasser.rachid.marsrovers.view;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import br.com.yasser.rachid.marsrovers.model.Command;
+import br.com.yasser.rachid.marsrovers.model.ExplorationPoint;
+
 public class Dialogs {
 	
 	private Scanner reader;
-	public static final String POSITION_INPUT_PATTERN = "([0-9])+(,)+([0-9])+";
+	public static final String FIELD_DIMENSION_INPUT_PATTERN = "([0-9])+(,)+([0-9])+";
+	public static final String POSITION_INPUT_PATTERN = "([0-9])+(,)+([0-9])+[NSWE]";
 	public static final String COMMAND_INPUT_PATTERN  = "[LMR]+";
+	
+	private static final int WIDTH_INDEX  = 0;
+	private static final int HEIGHT_INDEX = 1;
+	
 	
 	public Dialogs(Scanner reader){		
 		this.reader = reader;		
@@ -21,6 +29,20 @@ public class Dialogs {
 		return this.reader;
 	}
 	
+	public int[] askForFieldSize() {
+		System.out.println("field size [W,H] > ");
+		
+		String userInput = reader.next(FIELD_DIMENSION_INPUT_PATTERN);
+		
+		int[] dimensions = new int[2];
+		String [] inputDimensions = userInput.split(","); 
+		
+		dimensions[WIDTH_INDEX]  = Integer.parseInt(inputDimensions[WIDTH_INDEX]);
+		dimensions[HEIGHT_INDEX] = Integer.parseInt(inputDimensions[HEIGHT_INDEX]);
+		
+		return dimensions;				
+	}
+	
 	public ExplorationPoint askForPosition() throws InputMismatchException{		
 		System.out.print("position [X,Y] > " );
 		String input = "";
@@ -31,7 +53,7 @@ public class Dialogs {
 	}
 	
 	public Command askForCommand() throws InputMismatchException{
-		System.out.print("command [LMR] > ");
+		System.out.print("command [LRM] > ");
 		
 		String input = reader.next(COMMAND_INPUT_PATTERN);
 		Command command = new Command(input);
@@ -51,5 +73,5 @@ public class Dialogs {
 		point = new ExplorationPoint(x,y);
 		
 		return point;
-	}	
+	}
 }
