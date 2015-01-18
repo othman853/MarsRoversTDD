f18ca38c30ae25ba820f9e899837fc68e4965632
othman853 <anuar.yasser@gmail.com>
2015-01-18 18:45:41 -0200

    Robot class first steps

--- src/main/java/br/com/yasser/rachid/marsrovers/model/Robot.java
+++ src/main/java/br/com/yasser/rachid/marsrovers/model/Robot.java
@@ -1,5 +1,24 @@
 package br.com.yasser.rachid.marsrovers.model;
 
 public class Robot {
-
+	
+	private ExplorationPoint point;
+	private Orientation orientation;
+	
+	public Robot(ExplorationPoint point, Orientation orientation){
+		this.point = point;
+		this.orientation = orientation;
+	}
+	
+	public void toLeft(){
+		
+	}
+	
+	public void toRight(){
+		
+	}
+	
+	public void move(){
+		
+	}
 }
