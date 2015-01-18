422f19df1c396fd3b34f52cb97a37cf86df69d1c
othman853 <anuar.yasser@gmail.com>
2015-01-18 18:40:00 -0200

    Tests for Robot class created

---
+++ src/test/java/main/RobotTest.java
@@ -0,0 +1,54 @@
+package main;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.yasser.rachid.marsrovers.model.ExplorationPoint;
+import br.com.yasser.rachid.marsrovers.model.Robot;
+
+public class RobotTest {
+	
+	private Robot robot;
+	
+	@Before
+	public void setUp(){		
+		robot = new Robot();
+		
+		robot.setPoint(new ExplorationPoint(1,2));
+		robot.setOrientation(Orientation.NORTH);
+		
+	}
+	
+	@Test
+	public void toLeft_12NAsRobotPosition_ShouldChangePositionTo12W(){
+		String expectedPosition = "1 2 W";
+		
+		robot.toLeft();
+		String robotPosition = robot.toString();		
+		
+		assertThat(robotPosition, is(expectedPosition));
+	}
+	
+	@Test
+	public void toRight_12NAsRobotPosition_ShouldChangePositionTo12E(){
+		String expectedPosition = "1 2 E";
+		
+		robot.toRight();
+		String robotPosition = robot.toString();		
+		
+		assertThat(robotPosition, is(expectedPosition));		
+	}
+	
+	@Test
+	public void move_12NAsRobotPosition_ShouldChangePositionTo13N(){
+		String expectedPosition = "1 3 N";
+		
+		robot.toLeft();
+		String robotPosition = robot.toString();		
+		
+		assertThat(robotPosition, is(expectedPosition));		
+	}
+}
