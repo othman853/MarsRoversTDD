b2d7343b5fbdc88d1f00e20f15f7029e5d9ed596
othman853 <anuar.yasser@gmail.com>
2015-02-25 15:13:05 -0300

    Adding tests for addRobot() on MissionController

--- src/test/java/main/MissionControllerTest.java
+++ src/test/java/main/MissionControllerTest.java
@@ -2,6 +2,7 @@ package main;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.willThrow;
 import static org.mockito.Mockito.mock;
 
 import org.junit.Before;
@@ -13,6 +14,7 @@ import br.com.yasser.rachid.marsrovers.model.ExplorationField;
 import br.com.yasser.rachid.marsrovers.model.ExplorationPoint;
 import br.com.yasser.rachid.marsrovers.model.Orientation;
 import br.com.yasser.rachid.marsrovers.model.Robot;
+import br.com.yasser.rachid.marsrovers.model.exception.PointFilledException;
 
 public class MissionControllerTest {
 
@@ -27,13 +29,11 @@ public class MissionControllerTest {
 	}
 	
 	@Test
-	public void addRobot_12NAsInitialRobotPosition_ShouldAddTheRobotToTheList(){		
-		ExplorationPoint initialRobotPoint = new ExplorationPoint();
-		initialRobotPoint.setCoordinateX(1);
-		initialRobotPoint.setCoordinateY(2);
-		
+	public void addRobot_12NAsInitialRobotPosition_ShouldAddTheRobotToTheList() throws Exception{		
+		ExplorationPoint initialRobotPoint = new ExplorationPoint(1,2);		
 		Robot robot = new Robot(initialRobotPoint, Orientation.NORTH);		
 		
+		org.mockito.Mockito.when(field.getPointStatus(initialRobotPoint)).thenReturn(ExplorationPoint.POINT_FILLED);
 		controller.addRobot(robot);		
 		int pointStatus = controller.getField().getPointStatus(initialRobotPoint);
 		
@@ -41,8 +41,25 @@ public class MissionControllerTest {
 		assertThat(controller.getRobots().size(), is(1));
 	}
 	
-	@Test
-	public void addRobot_FilledPointOnFieldDestinationAsParam_Should(){
+	@Test(expected=PointFilledException.class)
+	public void addRobot_FilledPointOnFieldDestinationAsParam_ShouldThrowPointFilledException() throws PointFilledException{
+		ExplorationPoint initialPoint = new ExplorationPoint(1,2);
+		Robot robot = new Robot(initialPoint, Orientation.NORTH);
+		Robot robot2 = new Robot(initialPoint, Orientation.NORTH);
 		
+		willThrow(new PointFilledException()).given(field).addRobot(robot2);		
+		
+		controller.addRobot(robot);
+		controller.addRobot(robot2);		
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void addRobot_RobotOutOfFieldBoundsAsParam_ShouldThrowIllegalArgumentException() throws IllegalArgumentException, PointFilledException{
+		ExplorationPoint initialPoint = new ExplorationPoint(50,50);
+		Robot robot = new Robot(initialPoint, Orientation.NORTH);
+		
+		willThrow(new IllegalArgumentException()).given(field).addRobot(robot);
+		
+		controller.addRobot(robot);		
 	}
 }
